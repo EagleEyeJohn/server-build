09117945a1474df500751d2252a18903027f4c18
John Nicholls <john.nicholls@eagleeye.com>
2014-11-12 16:09:35 +0000

    acorn

--- public_html/index.php
+++ public_html/index.php
@@ -10,26 +10,7 @@
  */
 define('GITHUB_TOKEN', '78abce1f77af5e238a613653eab1b60d7787dbc2');
 
-// foreground colours
-$COL_BLACK   = "\x1b[30;01m";
-$COL_RED     = "\x1b[31;01m";
-$COL_GREEN   = "\x1b[32;01m";
-$COL_YELLOW  = "\x1b[33;01m";
-$COL_BLUE    = "\x1b[34;01m";
-$COL_MAGENTA = "\x1b[35;01m";
-$COL_CYAN    = "\x1b[36;01m";
-$COL_WHITE   = "\x1b[37;01m";
-
-// background colours
-$COL_BG_RED     = "\x1b[41;01m";
-$COL_BG_GREEN   = "\x1b[42;01m";
-$COL_BG_YELLOW  = "\x1b[43;01m";
-$COL_BG_BLUE    = "\x1b[44;01m";
-$COL_BG_MAGENTA = "\x1b[45;01m";
-$COL_BG_CYAN    = "\x1b[46;01m";
-$COL_BG_WHITE   = "\x1b[47;01m";
-
-$COL_RESET = "\x1b[39;49;00m";
+require dirname(__DIR__) . 'includes/ansi-colours.php';
 
 $git_clone = 'git clone https://' . GITHUB_TOKEN . ':x-oauth-basic@github.com/EagleEyeJohn/server-build.git';
 
@@ -53,17 +34,7 @@ else {
 
     $cmds[] = 'echo -e "' . $COL_BLACK . $COL_BG_CYAN . 'Hi ho! Hi ho! It\'s off to work we go! (' . $hostname . ')' . $COL_RESET . '"';
 
-    // Load commands to build settings that are common to all servers in this family
-    if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $family . '.php'))) {
-        $cmds[] = 'echo "Config from ' . $build . ' loaded"';
-        $cmds   = array_merge($cmds, require $path);
-    }
-
-    // Load commands to build settings that are specific to the server which invoked this script
-    if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $machine . '.php'))) {
-        $cmds[] = 'echo "Config from ' . $build . ' loaded"';
-        $cmds   = array_merge($cmds, require $path);
-    }
+    $cmds[] = 'php ' . dirname(__DIR__) . '/scripts/build.php';
 }
 
 echo implode(PHP_EOL, $cmds) . PHP_EOL;
---
+++ scripts/build.php
@@ -0,0 +1,20 @@
+<?php
+$hostname = php_uname();
+$machine  = explode('.', $hostname)[0];
+$family   = substr($machine, 0, 4);
+
+$cmds = [];
+
+// Load commands to build settings that are common to all servers in this family
+if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $family . '.php'))) {
+    $cmds[] = 'echo "Config from ' . $build . ' loaded"';
+    $cmds   = array_merge($cmds, require $path);
+}
+
+// Load commands to build settings that are specific to the server which invoked this script
+if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $machine . '.php'))) {
+    $cmds[] = 'echo "Config from ' . $build . ' loaded"';
+    $cmds   = array_merge($cmds, require $path);
+}
+
+print_r($cmds);
