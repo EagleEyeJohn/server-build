e2547c0a0202ec3e5ddb3798924be08733422521
John Nicholls <john.nicholls@eagleeye.com>
2014-11-12 16:35:44 +0000

    acorn

--- scripts/build.php
+++ scripts/build.php
@@ -10,13 +10,13 @@ $cmds = [];
 
 // Load commands to build settings that are common to all servers in this family
 if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $family . '.php'))) {
-    $cmds[] = 'echo "Config from ' . $build . ' loaded"';
+    $cmds[] = '# Config from "' . $build . '" loaded';
     $cmds   = array_merge($cmds, require $path);
 }
 
 // Load commands to build settings that are specific to the server which invoked this script
 if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $machine . '.php'))) {
-    $cmds[] = 'echo "Config from ' . $build . ' loaded"';
+    $cmds[] = '# Config from "' . $build . '" loaded';
     $cmds   = array_merge($cmds, require $path);
 }
 
