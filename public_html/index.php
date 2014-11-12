4624600566436cf9f9022ce83e916eca53f239ae
John Nicholls <john.nicholls@eagleeye.com>
2014-11-12 14:43:03 +0000

    acorn

--- public_html/index.php
+++ public_html/index.php
@@ -53,13 +53,13 @@ else {
 
     // Load commands to build settings that are common to all servers in this family
     if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $family . '.php'))) {
-        $cmds[] = 'echo "Config from "' . $build . '" loaded';
+        $cmds[] = 'echo "Config from ' . $build . ' loaded"';
         $cmds   = array_merge($cmds, require $path);
     }
 
     // Load commands to build settings that are specific to the server which invoked this script
     if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $family . '/' . $machine . '.php'))) {
-        $cmds[] = 'echo "Config from "' . $build . '" loaded';
+        $cmds[] = 'echo "Config from ' . $build . ' loaded"';
         $cmds   = array_merge($cmds, require $path);
     }
 }
