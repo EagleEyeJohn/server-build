8fae0707ccc428f2e16c29f3bad0d8beadf7335e
John Nicholls <john.nicholls@eagleeye.com>
2014-11-12 12:50:33 +0000

    acorn

---
+++ public_html/index.php
@@ -0,0 +1,19 @@
+<?php
+define('GITHUB_TOKEN', '78abce1f77af5e238a613653eab1b60d7787dbc2');
+
+$cmds = [];
+if (empty($_REQUEST['hostname'])) {
+    $cmds[] = 'curl ' . $_SERVER['HTTP_HOST'] . explode('?', $_SERVER['REQUEST_URI'])[0] . '?hostname={`hostname`}|sh';
+}
+else {
+    $hostname = $_REQUEST['hostname'];
+
+    $cmds[] = 'yum update -y';
+    $cmds[] = 'yum install git net-tools -y';
+    $cmds[] = 'cd /tmp';
+    $cmds[] = 'rm -Rf server-build';
+    $cmds[] = 'git clone https://' . GITHUB_TOKEN . ':x-oauth-basic@github.com/EagleEyeJohn/server-build.git';
+    $cmds[] = 'echo "Hi ho! Hi ho, it\'s off to work we go! ('. $hostname . ')"';
+}
+
+echo implode(PHP_EOL, $cmds) . PHP_EOL;
