843960a9a3a237a3313b4dec26d1aeef497eda49
John Nicholls <john.nicholls@eagleeye.com>
2014-11-18 08:48:27 +0000

    better http building

--- create/vhost/http.php
+++ create/vhost/http.php
@@ -16,8 +16,23 @@ file_put_contents('/etc/httpd/conf.d/vhost-00-default.conf', $vhost);
 file_put_contents('/var/www/index.html', <<<HTML
 <html>
 <title>Server {$hostname}</title>
+<style type="text/css">
+div{
+  background: red;
+  bottom: 0;
+  height: 10em;
+  left: 0;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 10em;
+}
+</style>
 <body>
+<div>
 <h1>{$hostname}</h1>
+</div>
 </body>
 </html>
 HTML
