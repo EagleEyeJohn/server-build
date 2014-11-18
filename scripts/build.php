5dedda8df35bc07edc351fdcc050a0885d92fa08
John Nicholls <john.nicholls@eagleeye.com>
2014-11-18 09:39:55 +0000

    better http building

--- scripts/build.php
+++ scripts/build.php
@@ -39,12 +39,12 @@ if (file_exists($path = dirname(__DIR__) . ($build = '/builds/' . $hostinfo['fam
 }
 
 // Load commands to build SSH certificate login details for Eagle Eye staff
+$cmds = [];
 try {
-    $cmds = [];
     $dir  = new DirectoryIterator(dirname(__DIR__) . '/ssh-keys');
     foreach ($dir as $file_info) {
         if (!$file_info->isDot()) {
-            $user   = $file_info->getFilename();
+            $user   = basename($file_info->getFilename(), '.pub');
             $cmds[] = 'useradd ' . $user;
             $cmds[] = 'mkdir ' . ($path = '/home/' . $user . '/.ssh');
             $cmds[] = 'chmod 0700 ' . $path;
@@ -55,7 +55,7 @@ try {
     runCommands($cmds);
 }
 catch (\Exception $e) {
-    $cmds = '# NO SSH KEYS';
+    $cmds[] = 'echo -e "' . COL_BLACK . COL_BG_RED . 'SSH KEY ERROR : "' .$e->getMessage() . COL_RESET . '"';
 }
 
 $cmds = [
